38855cb07814a61165b40b26aaa784c6521fda7d
cquentin26200 <qcheron@edenschool.fr>
2022-12-18 16:41:50 +0100

    add 404 page

---
+++ error/error404.php
@@ -0,0 +1,22 @@
+<?php $title = "404" ?>
+<?php include_once "../includes/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main class="error404">
+        <img src="../../todoList_v3/img/404.png" alt="">
+        <h1>Oops! This Page is Not Found.</h1>
+        <p>The requested page dose not exist.</p>
+        <a href="../index.php"><i class="fa-light fas fa-house"></i>Back to Home</a>
+    </main>
+</body>
+
+</html>
--- includes/header.php
+++ includes/header.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="../scss/style.css">
+    <link rel="stylesheet" href="../../todoList_v3/scss/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jost&family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/7a994d3c9f.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
